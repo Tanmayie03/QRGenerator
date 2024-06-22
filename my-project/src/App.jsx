078967b33aadcc28b8e5e5ff794c3ea5b1445fed
tanmayiepatil <120322328+Tanmayie03@users.noreply.github.com>
2024-06-22 22:24:36 +0530

    Changed the origin

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Qr code Generator</h1>;
+  return (
+    <h1 className="text-3xl font-bold text-center text-white underline bg-black">
+      Qr code Generator
+    </h1>
+  );
 }
 
 export default App;
