50d146eca79c5c4a8f3dc524140cfad381a25e9f
tanmayiepatil <120322328+Tanmayie03@users.noreply.github.com>
2024-06-23 17:15:00 +0530

    Added library and the project is on working stage

---
+++ my-project/components/BasicQr.jsx
@@ -0,0 +1,5 @@
+const BasicQr = () => {
+  return <div>BasicQr</div>;
+};
+
+export default BasicQr;
--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,49 @@
-function App() {
-  return (
-    <h1 className="text-3xl font-bold text-center text-white underline bg-black">
-      Qr code Generator
-    </h1>
+import React from "react";
+import { Button, Input, QRCode, Space } from "antd";
+
+const downloadQRCode = () => {
+  const canvas = document
+    .getElementById("igniteqrcode")
+    ?.querySelector("canvas");
+  if (canvas) {
+    const url = canvas.toDataURL("image/png");
+    const a = document.createElement("a");
+    a.download = "QRCode.png";
+    a.href = url;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+};
+
+const App = () => {
+  const [text, setText] = React.useState(
+    "https://www.ignitestudentassociation.com"
   );
-}
+  return (
+    <div id="igniteqrcode">
+      <Space type="canvas" className="flex flex-col items-center">
+        <QRCode
+          value={text || "-"}
+          bgColor="white"
+          icon="../src/assets/images/igniteFire.png"
+          bordered="true"
+          size={200}
+          level="H"
+          className="border-2 border-black "
+        />
+        <Input
+          color="black"
+          placeholder="Enter link"
+          className="text-black"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button type="primary" onClick={downloadQRCode}>
+          Download
+        </Button>
+      </Space>
+    </div>
+  );
+};
 
 export default App;
